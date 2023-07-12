da265b1d1b55b9675722370ae2cb5ce1275d2505
Song Hyunbin <shbin0519@gmail.com>
2023-07-12 14:55:42 +0900

    gradio inference

---
+++ Gradio_Inference/api.py
@@ -0,0 +1,112 @@
+import os
+import sys
+
+import torch
+import transformers
+from peft import PeftModel
+from transformers import GenerationConfig, GPTNeoXForCausalLM, GPTNeoXTokenizerFast
+
+from utils.callbacks import Iteratorize, Stream
+from utils.prompter import Prompter
+
+from flask import Flask, Response, request
+from flask_ngrok import run_with_ngrok
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+run_with_ngrok(app)
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'CARE_BOT'
+
+device = "cuda"
+load_8bit: bool = False
+base_model: str = "beomi/KoAlpaca-Polyglot-12.8B"
+lora_weights: str = "Final"
+prompt_template: str = "custom"
+
+prompter = Prompter(prompt_template)
+tokenizer = GPTNeoXTokenizerFast.from_pretrained(base_model)
+
+def load():
+    model = GPTNeoXForCausalLM.from_pretrained(
+        base_model,
+        load_in_8bit=load_8bit,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+    model = PeftModel.from_pretrained(
+        model,
+        lora_weights,
+        torch_dtype=torch.float16,
+    )
+
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+    model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+      
+    return model
+
+def evaluate(
+    instruction,
+    model,
+    input=None,
+    temperature=0.1,
+    top_p=0.75,
+    top_k=40,
+    num_beams=4,
+    max_new_tokens=128,
+    **kwargs,
+):
+    prompt = prompter.generate_prompt(instruction, input)
+    inputs = tokenizer(prompt, return_tensors="pt")
+    input_ids = inputs["input_ids"].to(device)
+    generation_config = GenerationConfig(
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+        num_beams=num_beams,
+        **kwargs,
+    )
+
+    # Without streaming
+    with torch.no_grad():
+        generation_output = model.generate(
+            input_ids=input_ids,
+            generation_config=generation_config,
+            return_dict_in_generate=True,
+            output_scores=True,
+            max_new_tokens=max_new_tokens,
+        )
+    s = generation_output.sequences[0]
+    output = tokenizer.decode(s)
+    yield prompter.get_response(output)
+
+@app.route('/api/main', methods=['POST'])
+def main():
+    response = Response()
+    if request.method == 'POST':
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        instruction = request.get_json()
+        responses = evaluate(instruction)
+        result = ""
+        for response in responses:
+            result += response
+        result = result.split("<|endoftext|>")[0]
+        result = result.split(instruction)[1]
+        result = result.replace('\n','')
+        response.set_data(json.dumps(result))
+        return response
+
+if __name__ == "__main__":
+    model = load()
+    app.run()
---
+++ Gradio_Inference/inference.py
@@ -0,0 +1,132 @@
+import os
+import sys
+
+import fire
+import torch
+import transformers
+from peft import PeftModel
+from transformers import GenerationConfig, GPTNeoXForCausalLM, GPTNeoXTokenizerFast
+
+from utils.callbacks import Iteratorize, Stream
+from utils.prompter import Prompter
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+
+def main(
+    load_8bit: bool = False,
+    base_model: str = "beomi/KoAlpaca-Polyglot-12.8B",
+    lora_weights: str = "Final",
+    prompt_template: str = "custom",  # The prompt template to use, will default to alpaca.
+):
+    base_model = base_model or os.environ.get("BASE_MODEL", "")
+    assert (
+        base_model
+    ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
+
+    prompter = Prompter(prompt_template)
+    tokenizer = GPTNeoXTokenizerFast.from_pretrained(base_model)
+    if device == "cuda":
+        model = GPTNeoXForCausalLM.from_pretrained(
+            base_model,
+            load_in_8bit=load_8bit,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            torch_dtype=torch.float16,
+        )
+    elif device == "mps":
+        model = GPTNeoXForCausalLM.from_pretrained(
+            base_model,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = GPTNeoXForCausalLM.from_pretrained(
+            base_model, device_map={"": device}, low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+        )
+
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+
+    if not load_8bit:
+        model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    def evaluate(
+        instruction,
+        input=None,
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        max_new_tokens=128,
+        **kwargs,
+    ):
+        prompt = prompter.generate_prompt(instruction, input)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
+
+        # Without streaming
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output = tokenizer.decode(s)
+        yield prompter.get_response(output)
+    
+   # testing code for readme
+    instruction = [
+      "나 오늘 기분이 안좋아.",
+      "너 이름이 뭐야?",
+      "점심 메뉴 추천해줘."
+    ]
+    for item in instruction:
+        print("Instruction:", item)
+        responses = evaluate(item)
+        result = ""
+        for response in responses:
+            result += response
+        result = result.split("<|endoftext|>")[0]
+        result = result.split(item)[1]
+        result = result.replace('\n','')
+        print(result)
+ 
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
