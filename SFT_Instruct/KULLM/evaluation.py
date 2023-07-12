a88d97c6f2ebe628372aeffecb217078c89c0b9b
Song Hyunbin <shbin05@naver.com>
2023-07-13 07:53:07 +0900

    change litle evaluation

--- SFT_Instruct/KULLM/evaluation.py
+++ SFT_Instruct/KULLM/evaluation.py
@@ -1,31 +1,68 @@
 import pandas as pd
-import json
 import openai
-from tqdm import tqdm
-import jsonlines
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, pipeline
+from peft import PeftModel
+from utils.prompter import Prompter
 
 openai.api_key = "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"
 
-# id 정보를 모아서 저장하는 예시
-data_list= []
-prompt_format = {
-    "instruction" : "test instruction",
-    "input" : "",
-    "output" : "test output"
-} 
+MODEL = "nlpai-lab/kullm-polyglot-12.8b-v2"
 
-def make_response(instruction) :
-    response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [{"role": "system", "content": "두 사람 간의 대화가 주어집니다. 다음의 지시문(Instruction)을 받게 될 것입니다.\
-                                                당신의 작업은 지시문과 입력에 해당하는 답변(response)을 생성하는 것입니다.\
-                                                너무 길지 않고 간결하게 답변을 생성해주세요."},
-                    {'role':'user','content': f'\
-                     Instruction: {instruction}\
-                     Response: '},
-                    ],
-        temperature = 0.5)
-    return response['choices'][0]['message']['content']
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True,
+).to(device=f"cuda", non_blocking=True)
+model = PeftModel.from_pretrained(
+    model,
+    "conversation",
+    torch_dtype=torch.float16,
+).to(device=f"cuda", non_blocking=True)
+model.eval()
+
+prompter = Prompter("kullm")
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+pipe = pipeline("text-generation", model=model, tokenizer=MODEL, device=0)
+
+def infer_from_original(instruction="", input_text=""):
+    prompt = prompter.generate_prompt(instruction, input_text)
+    output = pipe(prompt, max_length=512, temperature=0.2, num_beams=5, eos_token_id=2)
+    s = output[0]["generated_text"]
+    result = prompter.get_response(s)
+
+    return result
+
+def infer_from_fintuned(
+    instruction,
+    input=None,
+    temperature=0.2,
+    num_beams=5,
+    max_new_tokens=512,
+    **kwargs,
+):
+    prompt = prompter.generate_prompt(instruction, input)
+    inputs = tokenizer(prompt, return_tensors="pt")
+    input_ids = inputs["input_ids"].to(device=f"cuda")
+    generation_config = GenerationConfig(
+        temperature=temperature,
+        num_beams=num_beams,
+        **kwargs,
+    )
+
+    # Without streaming
+    with torch.no_grad():
+        generation_output = model.generate(
+            input_ids=input_ids,
+            generation_config=generation_config,
+            return_dict_in_generate=True,
+            output_scores=True,
+            max_new_tokens=max_new_tokens,
+        )
+    s = generation_output.sequences[0]
+    output = tokenizer.decode(s)
+    yield prompter.get_response(output)
 
 
 def make_evaluation(instruction, output) :
@@ -72,24 +109,34 @@ df = pd.DataFrame(columns=COLUMNS)
 # 데이터 불러오기
 with open('./conversation_prompt', 'r', encoding='utf-8') as f:
     prompts = f.readlines()
+    
+prompt_format = {
+    "instruction" : "test instruction",
+    "input" : "",
+    "output" : "test output"
+} 
 
 count = 0
 for prompt in prompts:
     instruction = prompt
-    print(instruction)
     prompt_format['instruction'] = instruction    
     if count > 100: break # 100개만 돌리고 끝
     try:        
-        prompt_format['output'] = make_response(instruction)        
-        score = extract_scores_from_string(make_evaluation(prompt_format['instruction'],prompt_format['output']))
-        print(f"instruction : {prompt_format['instruction']}")
-        print(f"output : {prompt_format['output']}")
-        print(f"evaluation: {score}\n\n")
+        output = infer_from_fintuned(instruction)
+        result=""
+        for s in output:
+            result+=s
+        output = result.split("<|endoftext|>​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​")[0]
+        prompt_format['output'] = output
+        score = extract_scores_from_string(make_evaluation(instruction, output))
+        print(f"instruction : {instruction}")
+        print(f"output : {output}")
+        print(f"evaluation: {score}\n")
         newDF = list()
         newDF = score
         newDF = pd.DataFrame(data=[newDF], columns = COLUMNS) 
         df = pd.concat([df,newDF])       
-        df.to_csv("/content/drive/MyDrive/")  
+        df.to_csv("/content/drive/MyDrive/kullm_conversation_eval.csv")  
         count += 1
     except:
         print("error occur!")
