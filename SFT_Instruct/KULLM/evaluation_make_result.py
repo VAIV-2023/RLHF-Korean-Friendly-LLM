ce01806ed4a33a0dd411765e1d4928e669dc53e4
허유민 <YM@heohoejun-ui-MacBookAir.local>
2023-07-18 16:32:53 +0900

    eval

--- SFT_Instruct/KULLM/evaluation_make_result.py
+++ SFT_Instruct/KULLM/evaluation_make_result.py
@@ -16,8 +16,9 @@ parser.add_argument("--peft_model", type=str, default=None)
 parser.add_argument("--openai_key", type=str, default="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")
 parser.add_argument("--base_model", type=str, default="nlpai-lab/kullm-polyglot-12.8b-v2")
 parser.add_argument("--dataset", type=str, default=None)
-parser.add_argument("--output_file", type=str, default='./output/out.csv')
+parser.add_argument("--output_file", type=str, default='out.csv')
 parser.add_argument("--gpt", type=bool, default=True)
+parser.add_argument("--prompt_only", type=str, default=False)
 
 args=parser.parse_args()
 
@@ -60,63 +61,6 @@ def infer_from_original(instruction="", input_text=""):
     result = prompter.get_response(s)
     return result
 
-
-# 데이터 불러오기
-prompts=[]
-dataset_type='prompt_only'
-with open(f'./{args.dataset}', 'r', encoding='utf-8') as f:
-    if os.path.splitext(args.dataset)[1]=='.jsonl':
-        for line in f:
-            json_data = json.loads(line)
-            if dataset_type=='prompt_only':
-                prompts.append(json_data['instruction'])
-            else:
-                for inst in json_data['instances']:
-                    prompts.append({'instruction': json_data['instruction'], 'input' : inst['input'] })
-    else:
-        prompts = f.readlines()
-
-COLUMNS = ['instruction', 'input', 'prompt', 'model_output' ,'base_model_output', 'gpt3_output']
-df = pd.DataFrame(columns=COLUMNS)
-count = 0
-for prompt in prompts:
-    if dataset_type=='prompt_only':
-        instruction=prompt
-        input_text=None
-    else:
-        instruction=prompt['instruction']
-        input_text=prompt['input']
-    backoff_time = 10
-    retry_cnt=0
-    row = {'instruction':instruction, 'input': input_text, 'prompt': prompter.generate_prompt(instruction=instruction, input=input_text)}
-    if args.gpt:
-        while retry_cnt<10:
-            try:
-                output = infer_from_gpt(instruction, input_text)
-            except openai.error.OpenAIError as e:
-                if "Please reduce your prompt" in str(e):
-                    target_length = int(target_length * 0.8)
-                    print(f"Reducing target length to {target_length}, retrying...")
-                else:
-                    print(f"Retrying in {backoff_time} seconds...")
-                    time.sleep(backoff_time)
-                    backoff_time *= 1.5
-                    retry_cnt+=1
-                output=str(e)
-        row['gpt3_output']=output
-    try:
-        output = infer_from_original( instruction=instruction, input_text=input_text)
-    except Exception as e:
-        output=str(e)
-    row['base_model_output']=output
-    # score = extract_scores_from_string(make_evaluation(instruction, output))
-    # print(f"instruction : {instruction}")
-    # print(f"output : {output}")
-    # print(f"evaluation: {score}\n")
-    df.append(row,ignore_index=True)
-    count += 1
-
-
 def infer_from_fintuned(
     instruction,
     input=None,
@@ -148,34 +92,93 @@ def infer_from_fintuned(
     yield prompter.get_response(output)
 
 
-if finetuned:
-    model = PeftModel.from_pretrained(
-        model,
-        args.peft_model,
-        torch_dtype=torch.float16,
-    ).to(device=f"cuda", non_blocking=True)
-    
-    tokenizer = AutoTokenizer.from_pretrained(MODEL)
-    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
-    model.config.bos_token_id = 1
-    model.config.eos_token_id = 2
-    idx=0
+# 데이터 불러오기
+prompts=[]
+prompt_only=args.prompt_only
+with open(f'./{args.dataset}', 'r', encoding='utf-8') as f:
+    if os.path.splitext(args.dataset)[1]=='.jsonl':
+        for line in f:
+            json_data = json.loads(line)
+            if prompt_only:
+                prompts.append(json_data['instruction'])
+            else:
+                for inst in json_data['instances']:
+                    prompts.append({'instruction': json_data['instruction'], 'input' : inst['input'] })
+    else:
+        prompts = f.readlines()
+
+COLUMNS = ['instruction', 'input', 'prompt', 'model_output' ,'base_model_output', 'gpt3_output']
+df = pd.DataFrame(columns=COLUMNS)
+count = 0
+try:
     for prompt in prompts:
-        if dataset_type=='prompt_only':
+        if prompt_only:
             instruction=prompt
             input_text=None
         else:
             instruction=prompt['instruction']
             input_text=prompt['input']
+        backoff_time = 10
+        retry_cnt=0
+        row = {'instruction':instruction, 'input': input_text, 'prompt': prompter.generate_prompt(instruction=instruction, input=input_text)}
+        if args.gpt:
+            while retry_cnt<3:
+                try:
+                    output = infer_from_gpt(instruction, input_text)
+                except openai.error.OpenAIError as e:
+                    print(str(e))
+                    if "Please reduce your prompt" in str(e):
+                        target_length = int(target_length * 0.8)
+                        print(f"Reducing target length to {target_length}, retrying...")
+                    else:
+                        print(f"Retrying in {backoff_time} seconds...")
+                        time.sleep(backoff_time)
+                        backoff_time *= 1.5
+                        retry_cnt+=1
+                    output=str(e)
+            row['gpt3_output']=output
         try:
-            output = infer_from_fintuned(instruction=instruction, input=input_text)
+            output = infer_from_original( instruction=instruction, input_text=input_text)
         except Exception as e:
             output=str(e)
-        df.loc[idx,'model_output']=output
-        idx+=1
+        row['base_model_output']=output
+        print(f'@@@{instruction} ### {input_text} ### {prompt}\n###{output}')
+        # score = extract_scores_from_string(make_evaluation(instruction, output))
+        # print(f"instruction : {instruction}")
+        # print(f"output : {output}")
+        # print(f"evaluation: {score}\n")
+        df.append(row,ignore_index=True)
+        count += 1
 
-
-df.to_csv(f"./{args.output_file}", encoding='utf-8')
+    if finetuned:
+        model = PeftModel.from_pretrained(
+            model,
+            args.peft_model,
+            torch_dtype=torch.float16,
+        ).to(device=f"cuda", non_blocking=True)
+        
+        tokenizer = AutoTokenizer.from_pretrained(MODEL)
+        model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+        model.config.bos_token_id = 1
+        model.config.eos_token_id = 2
+        idx=0
+        for prompt in prompts:
+            if prompt_only:
+                instruction=prompt
+                input_text=None
+            else:
+                instruction=prompt['instruction']
+                input_text=prompt['input']
+            try:
+                output = infer_from_fintuned(instruction=instruction, input=input_text)
+            except Exception as e:
+                output=str(e)
+            df.loc[idx,'model_output']=output
+            print(f'@@@{instruction} ### {input_text} ### {prompt}\n###{output}')
+            idx+=1
+    df.to_csv(f"./output/{time.strftime('%m-%d-%H-%M-%S')}_{args.output_file}", encoding='utf-8')
+except:
+    df.to_csv(f"./output/{time.strftime('%m-%d-%H-%M-%S')}_{args.output_file}", encoding='utf-8')
     
 """
 {'role':'user','content': '평가 기준:\
