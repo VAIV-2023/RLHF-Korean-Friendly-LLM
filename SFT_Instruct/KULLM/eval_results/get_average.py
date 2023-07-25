48f24b4441205f309eb280afe52b61d5057178bc
Song Hyunbin <shbin0519@gmail.com>
2023-07-25 18:46:47 +0900

    rm directory created

--- SFT_Instruct/KULLM/eval_results/get_average.py
+++ SFT_Instruct/KULLM/eval_results/get_average.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 # CSV 파일을 읽어서 DataFrame 객체로 변환
-data = pd.read_csv('./all/kullm_orig_all_eval.csv')
+data = pd.read_csv('./all/gpt_all_eval.csv')
 
 # 원하는 열 선택
 columns = ['친근함', '무해함', '이해 가능성', '자연스러움', '맥락 유지', '전반적인 품질']
 selected_data = data[columns]
+print(len(selected_data))
 
 # 열의 평균값 계산
 avg = selected_data.mean()
