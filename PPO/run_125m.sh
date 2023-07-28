14e438a549cad867a0249385aff8c93016788b9a
Seongwan <waniboyy@gmail.com>
2023-07-28 19:41:40 +0900

    [ADD] Step3 dataset

---
+++ PPO/run_125m.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+ACTOR_ZERO_STAGE="--actor_zero_stage 0"
+CRITIC_ZERO_STAGE="--critic_zero_stage 0"
+
+OUTPUT="./output"
+
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./output
+fi
+if [ "$ACTOR_ZERO_STAGE" == "" ]; then
+    ACTOR_ZERO_STAGE=0
+fi
+if [ "$CRITIC_ZERO_STAGE" == "" ]; then
+    CRITIC_ZERO_STAGE=0
+fi
+
+Num_Padding_at_Beginning=1 # this is model related
+
+Actor_Lr=5e-4
+Critic_Lr=5e-6
+
+mkdir -p $OUTPUT
+
+deepspeed --num_gpus 1 main.py \
+   --data_path Dahoas/rm-static \
+   --data_split 2,4,4 \
+   --actor_model_name_or_path /content/drive/MyDrive/output/actor-models/125m \
+   --critic_model_name_or_path /content/drive/MyDrive/output/reward-models/125m \
+   --num_padding_at_beginning 1 \
+   --per_device_train_batch_size 8 \
+   --per_device_mini_train_batch_size 8 \
+   --generation_batch_numbers 1 \
+   --release_inference_cache \
+   --ppo_epochs 1 \
+   --max_answer_seq_len 256 \
+   --max_prompt_seq_len 256 \
+   --actor_learning_rate ${Actor_Lr} \
+   --critic_learning_rate ${Critic_Lr} \
+   --num_train_epochs 1 \
+   --lr_scheduler_type cosine \
+   --gradient_accumulation_steps 16 \
+   --num_warmup_steps 100 \
+   --deepspeed --seed 1234 \
+   ${ACTOR_ZERO_STAGE} \
+   ${CRITIC_ZERO_STAGE} \
+   --actor_lora_dim 128 \
+   --critic_lora_dim 128 \
+   --print_answers \
+   --disable_actor_dropout \
+   --enable_hybrid_engine \
+   --actor_gradient_checkpointing \
+   --critic_gradient_checkpointing \
+   --offload_reference_model \
+   --enable_tensorboard \
+   --tensorboard_path /content/drive/MyDrive/output/tensorboard.log \
+   --output_dir $OUTPUT \
+    2>&1 | tee "$OUTPUT/training.log"
